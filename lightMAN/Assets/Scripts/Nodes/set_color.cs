c47052fa14e8beb2a50902aea7a77080520cd690
DerwesX <2025lobanov.ss@student.letovo.ru>
2024-03-25 21:09:01 +0300

    still better rgb node

--- lightMAN/Assets/Scripts/Nodes/set_color.cs
+++ lightMAN/Assets/Scripts/Nodes/set_color.cs
@@ -14,6 +14,7 @@ public class set_color : default_node
 	
     [SerializeField] Texture2D colorChart;
     [SerializeField] GameObject chart;
+    [SerializeField] GameObject chosenPoint;
 
     public void PickColor()
     {
@@ -21,7 +22,10 @@ public class set_color : default_node
 		local_position.x += chart.transform.GetComponent<RectTransform>().rect.width / 2;
 		local_position.y += chart.transform.GetComponent<RectTransform>().rect.height / 2;
     	Color pickedColor = colorChart.GetPixel((int)(local_position.x * (colorChart.width / chart.transform.GetComponent<RectTransform>().rect.width)), (int)(local_position.y * (colorChart.height / chart.transform.GetComponent<RectTransform>().rect.height)));
-    	node_body.GetComponent<Renderer>().material.SetColor("_EmissionColor", pickedColor);
+	    Vector3 new_point_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	    new_point_position.z = -0.15f;
+	    chosenPoint.transform.position = new_point_position;
+	    node_body.GetComponent<Renderer>().material.SetColor("_EmissionColor", pickedColor);
         data["R"] = ((int)(pickedColor.r * 255)).ToString();
         data["G"] = ((int)(pickedColor.g * 255)).ToString();
         data["B"] = ((int)(pickedColor.b * 255)).ToString();
